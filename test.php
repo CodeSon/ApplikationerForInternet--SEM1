1c4473c78f73df847f6148c60c1e5759392c7be4
David Nartey <dnartey@kth.se>
2015-09-01 22:19:55 +0200

    Testing PHP
    Just Testing

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+	$myArray=array("pizza","chocolate","coffee");
+	print_r($myArray);
+	echo $myArray[2];
+	
+	echo "<br/><br/>";
+	
+	$thirdArray=array("France"=> "French",
+					
+					"Kumasi"=>"Ashanti",
+					"USA"=>"English",
+					"SWEDEN"=>"Svenska",
+					"SOUTH AFRICA"=>"Swahili",
+					"GERMANY"=>"German"
+					);
+					
+					print_r($thirdArray);
+
+?>
